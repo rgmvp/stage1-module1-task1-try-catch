1459ba813af54b8c88952fd391cd356100160c62
rgmvp <perez4horoneniepwnz@yandex.ru>
2023-02-05 20:57:36 +0300

    done

--- src/main/java/com/epam/m1/exceptions/ParseIntegers.java
+++ src/main/java/com/epam/m1/exceptions/ParseIntegers.java
@@ -65,7 +65,7 @@ public class ParseIntegers {
 
 
         System.out.println("Sum is " + sum);
-        System.out.println("Just words:" + justWords.trim());
+        System.out.println("Just words: " + justWords.trim());
     }
 }
 
